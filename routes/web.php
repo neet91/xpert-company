0bb5784e7a547f15a7c70d005c555a63ea1647a1
neet91 <harithtajudin91@gmail.com>
2026-06-19 17:55:36 +0800

    test commit

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'Welcome')->name('home');
 
 Route::prefix('{current_team}')
-    ->middleware(['auth', EnsureTeamMembership::class])//'verified'
+    ->middleware(['auth', EnsureTeamMembership::class])//'verified' //test
     ->group(function () {
         Route::get('dashboard', DashboardController::class)->name('dashboard');
     });
